685dc808504a3018c920ce55f424d1b1cd6b2490
Kajakwankhede <Kajalwankhede@example.com>
2022-10-10 15:03:05 +0530

    My Day5 Practice Problem

--- src/day5/OddEven.java
+++ src/day5/OddEven.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class OddEven {
     void display() {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter the number: ");
+        System.out.print("Enter the number : ");
         int num = reader.nextInt();
         if (num % 2 == 0)
             System.out.println(num + " is even");
