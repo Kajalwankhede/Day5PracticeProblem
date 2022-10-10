950b3f7cfff157e852a7cb97f46187d2ac958bb9
Kajakwankhede <Kajalwankhede@example.com>
2022-10-10 14:56:06 +0530

    Added My Day5 Practice Problem

--- src/day5/Vowelconsonant.java
+++ src/day5/Vowelconsonant.java
@@ -7,22 +7,22 @@ public class Vowelconsonant {
         char alphabet = sc.next().charAt(0);
         switch (alphabet) {
             case 'a':
-                System.out.println("alphabet = a is, a vowel");
+                System.out.println("Alphabet = a is, a vowel");
                 break;
             case 'e':
-                System.out.println("alphabet = e is, a vowel");
+                System.out.println("Alphabet = e is, a vowel");
                 break;
             case 'i':
-                System.out.println("alphabet = i is, a vowel");
+                System.out.println("Alphabet = i is, a vowel");
                 break;
             case 'o':
-                System.out.println("alphabet = o is, a vowel");
+                System.out.println("Alphabet = o is, a vowel");
                 break;
             case 'u':
-                System.out.println("alphabet = u is, a vowel");
+                System.out.println("Alphabet = u is, a vowel");
                 break;
             default:
-                System.out.println("alphabet = " + alphabet + " is, a consonant");
+                System.out.println("Alphabet = " + alphabet + " is, a consonant");
                 break;
         }
 
